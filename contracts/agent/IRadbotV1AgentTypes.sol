b986cc1c2f4c5d76344e2e430529af255fc4dbe8
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-29 13:19:44 -0700

    agent-chore-3

--- contracts/agent/IRadbotV1AgentTypes.sol
+++ contracts/agent/IRadbotV1AgentTypes.sol
@@ -2,17 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IRadbotV1AgentTypes {
-    struct AgentParams {
-        address owner;
-        address deployer;
-        uint256 maxAgents;
-        bytes32 name;
-        bytes16 symbol;
-        string description;
-        string baseURI;
-        uint16 royalty;
-    }
-
     struct AgentTraits {
         uint256 deployments;
         uint256 yield;
