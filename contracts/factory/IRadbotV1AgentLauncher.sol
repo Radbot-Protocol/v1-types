414602531c2fbd4a9805f603ba993ee5d875086c
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-29 15:42:33 -0700

    factory-refactor

---
+++ contracts/factory/IRadbotV1AgentLauncher.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IRadbotV1AgentLauncher {
+    function parameters()
+        external
+        view
+        returns (
+            address owner,
+            uint256 mintPrice,
+            uint256 maxAgents,
+            bytes32 name,
+            bytes16 symbol,
+            string memory description,
+            string memory baseURI,
+            uint16 royalty,
+            address factory
+        );
+}
