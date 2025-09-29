094994397a77572e7290f75d4656bba62172e0ea
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-29 15:09:25 -0700

    agent-chore-5

--- contracts/agent/IRadbotV1AgentOwnerAction.sol
+++ contracts/agent/IRadbotV1AgentOwnerAction.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 
 interface IRadbotV1AgentOwnerAction {
     function withdraw(address token, address to, uint256 amount) external;
+
+    function updateBaseURI(string calldata baseURI) external;
 }
