852523be0b2c4b600389d1c46dc628de45b68018
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-29 12:34:24 -0700

    factory-chore-2

--- contracts/factory/IRadbotV1FactoryOwnerAction.sol
+++ contracts/factory/IRadbotV1FactoryOwnerAction.sol
@@ -1,6 +1,4 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.0;
 
-interface IRadbotV1FactoryOwnerAction {
-    function setDeployer(address deployer) external;
-}
+interface IRadbotV1FactoryOwnerAction {}
