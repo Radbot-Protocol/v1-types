11c0528442c4ac1ec674fee9cc92d2434b657b46
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-29 15:44:49 -0700

    factory-refactor

--- contracts/IRadbotV1Factory.sol
+++ contracts/IRadbotV1Factory.sol
@@ -5,7 +5,7 @@ import {IRadbotV1FactoryAction} from "./factory/IRadbotV1FactoryAction.sol";
 import {IRadbotV1FactoryImmutable} from "./factory/IRadbotV1FactoryImmutable.sol";
 import {IRadbotV1FactoryOwnerAction} from "./factory/IRadbotV1FactoryOwnerAction.sol";
 import {IRadbotV1FactoryState} from "./factory/IRadbotV1FactoryState.sol";
-import {IRadbotV1AgentLancher} from "./factory/IRadbotV1AgentLancher.sol";
+import {IRadbotV1AgentLauncher} from "./factory/IRadbotV1AgentLauncher.sol";
 import {IRadbotV1FactoryPayment} from "./factory/IRadbotV1FactoryPayment.sol";
 
 interface IRadbotV1Factory is
@@ -13,6 +13,6 @@ interface IRadbotV1Factory is
     IRadbotV1FactoryImmutable,
     IRadbotV1FactoryOwnerAction,
     IRadbotV1FactoryState,
-    IRadbotV1AgentLancher,
+    IRadbotV1AgentLauncher,
     IRadbotV1FactoryPayment
 {}
