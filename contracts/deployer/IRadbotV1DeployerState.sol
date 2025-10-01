4421a03869071e771c218efc88e2d3f0ce89edae
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-09-30 21:42:48 -0700

    deployer-chore-1

--- contracts/deployer/IRadbotV1DeployerState.sol
+++ contracts/deployer/IRadbotV1DeployerState.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.0;
 
 interface IRadbotV1DeployerState {
     function fee() external view returns (uint256);
+
+    function balance(address token) external view returns (uint256);
 }
