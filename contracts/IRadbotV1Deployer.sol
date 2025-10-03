729002edf38f2dccfdc553a1c590f2ff91b9cfa7
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-03 10:02:12 -0700

    deployer-chore-2

--- contracts/IRadbotV1Deployer.sol
+++ contracts/IRadbotV1Deployer.sol
@@ -5,10 +5,16 @@ import {IRadbotV1DeployerAction} from "./deployer/IRadbotV1DeployerAction.sol";
 import {IRadbotV1DeployerImmutable} from "./deployer/IRadbotV1DeployerImmutable.sol";
 import {IRadbotV1DeployerOwnerAction} from "./deployer/IRadbotV1DeployerOwnerAction.sol";
 import {IRadbotV1DeployerState} from "./deployer/IRadbotV1DeployerState.sol";
+import {IRadbotV1DeployerTypes} from "./deployer/IRadbotV1DeployerTypes.sol";
+import {IRadbotV1DeployerErrors} from "./deployer/IRadbotV1DeployerErrors.sol";
+import {IRadbotV1DeployerEvents} from "./deployer/IRadbotV1DeployerEvents.sol";
 
 interface IRadbotV1Deployer is
     IRadbotV1DeployerAction,
     IRadbotV1DeployerImmutable,
     IRadbotV1DeployerOwnerAction,
-    IRadbotV1DeployerState
+    IRadbotV1DeployerState,
+    IRadbotV1DeployerTypes,
+    IRadbotV1DeployerErrors,
+    IRadbotV1DeployerEvents
 {}
