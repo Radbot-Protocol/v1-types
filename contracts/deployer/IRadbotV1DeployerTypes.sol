0b3f3925757264ab85c9e33b400acac8dc2f7a9f
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-04 10:49:49 -0700

    deployer-chore-4

--- contracts/deployer/IRadbotV1DeployerTypes.sol
+++ contracts/deployer/IRadbotV1DeployerTypes.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 interface IRadbotV1DeployerTypes {
     struct DeployData {
         address payer;
+        address recipient;
         bytes32 name;
         bytes16 symbol;
         uint256 maxAgents;
