a16b7bcbcfcdd415c5fcd4e86e6e38a79799a45a
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-05 00:29:39 -0700

    deprecate-updateActiveUserStatus

--- contracts/user/IRadbotV1UserKeeperAction.sol
+++ contracts/user/IRadbotV1UserKeeperAction.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.0;
 
 interface IRadbotV1UserKeeperAction {
-    /// @notice Updates the active status of a user
-    /// @param _user The address of the user
-    /// @param _isActive Whether the user should be active or inactive
-    function updateActiveUserStatus(address _user, bool _isActive) external;
+    // /// @notice Updates the active status of a user
+    // /// @param _user The address of the user
+    // /// @param _isActive Whether the user should be active or inactive
+    // function updateActiveUserStatus(address _user, bool _isActive) external;
 }
