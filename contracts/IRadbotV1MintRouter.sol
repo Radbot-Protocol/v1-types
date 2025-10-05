ce752fe799257493c6c9cf9b2c34c1c5ac9f02f2
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-05 00:02:33 -0700

    add-mint-router

---
+++ contracts/IRadbotV1MintRouter.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.0;
+
+import {IRadbotV1MintRouterAction} from "./router/IRadbotV1MintRouterAction.sol";
+
+interface IRadbotV1MintRouter is IRadbotV1MintRouterAction {}
