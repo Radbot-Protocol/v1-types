44fe3836d855417108980b330af0e89c9e7dbe30
radbot <211224137+bxkdhc6282@users.noreply.github.com>
2025-10-05 00:22:52 -0700

    mint-router-chore-1

--- contracts/router/IRadbotV1MintRouterAction.sol
+++ contracts/router/IRadbotV1MintRouterAction.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 interface IRadbotV1MintRouterAction {
     struct MintInputParams {
         address to;
+        address payer;
         address token;
         bytes32 name;
         bytes16 symbol;
